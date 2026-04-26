6d2779c547caa873300e3689319536c03e21aecc
selogersn@gmail.com <selogersn@gmail.com>
2026-04-26 02:45:45 +0000

    Fix: Correct Payload 3.0 exports (RootPage from views, individual REST handlers) and fix config paths

--- app/(payload)/admin/[[...payload]]/page.tsx
+++ app/(payload)/admin/[[...payload]]/page.tsx
@@ -1,4 +1,4 @@
-import { RootPage } from '@payloadcms/next/layouts'
+import { RootPage } from '@payloadcms/next/views'
 import config from '../../../../payload.config'
 
 const Page = ({ params, searchParams }: any) => RootPage({ config, params, searchParams })
